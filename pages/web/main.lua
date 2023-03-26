2bb2a260ea77434321935a92767134f43a6a91a3
机不可失 <126877770+jibukeshi@users.noreply.github.com>
2023-03-26 18:25:12 +0800

    Add files via upload

--- pages/web/main.lua
+++ pages/web/main.lua
@@ -5,6 +5,7 @@ import "android.view.*"
 import "android.app.*"
 import "android.webkit.WebChromeClient"
 import "android.webkit.WebViewClient"
+import "android.webkit.DownloadListener"
 
 UiManager=activity.UiManager
 Colors=UiManager.Colors
@@ -53,6 +54,7 @@ function onMenuItemClick(title)
     activity.startActivity(Intent("android.intent.action.VIEW",Uri.parse(获取网址())))
   end
 end
+
 UiManager.currentFragment.webView.setWebViewClient(luajava.override(WebViewClient,{
   shouldOverrideUrlLoading=function(superCall,view,webResourceRequest)
     --即将开始加载事件
@@ -84,15 +86,15 @@ UiManager.currentFragment.webView.setWebViewClient(luajava.override(WebViewClien
     --页面资源加载监听
     --可通过该方法获取网页上的资源
   end,
-  onReceivedSslError=function(superCall, view, sslErrorHandler, sslError)
+  onReceivedSslError=function(superCall,view,sslErrorHandler,sslError)
     --ssl证书错误处理事件
     --需自行处理，请返回true拦截原事件
     return true
   end,
-  onReceivedHttpError=function(superCall, view, webResourceRequest, webResourceResponse)
+  onReceivedHttpError=function(superCall,view,webResourceRequest,webResourceResponse)
     --请求返回HTTP错误码时
   end,
-  onReceivedError=function(superCall, view, webResourceRequest,webResourceError)
+  onReceivedError=function(superCall,view,webResourceRequest,webResourceError)
     --页面加载异常事件
   end,
 }))
@@ -108,3 +110,53 @@ UiManager.currentFragment.webView.setWebChromeClient(luajava.override(WebChromeC
     --网页加载进度变化
   end,
 }))
+
+UiManager.currentFragment.webView.setDownloadListener(DownloadListener{
+  onDownloadStart=function(url,user_agent,contentDisposition,mimeType,content_length)
+    -- 监听下载
+    import "java.net.URLDecoder"
+    import "android.text.format.Formatter"
+    import "android.webkit.URLUtil"
+    local filename=URLDecoder.decode(URLUtil.guessFileName(url,contentDisposition,mimeType))
+    local size=Formatter.formatFileSize(activity,content_length)
+    local dialog=AlertDialog.Builder(this)
+    .setTitle("下载文件")
+    .setMessage("文件名："..filename.."\n文件类型："..mimeType.."\n文件大小："..size.."\n下载链接："..url)
+    .setPositiveButton("内部下载",function()
+      import "android.content.Context"
+      import "android.net.Uri"
+      import "android.service.voice.VoiceInteractionSession$Request"
+      local path=activity.getSharedData("下载目录")
+      if(path:find("/sdcard"))then
+        path=path:match("/sdcard(.+)")
+      end
+      if(path:find("/storage/emulated/0"))then
+        path=path:match("/storage/emulated/0(.+)")
+      end
+      local downloadManager=activity.getSystemService(Context.DOWNLOAD_SERVICE)
+      local request=DownloadManager.Request(Uri.parse(url))
+      request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)--设置通知栏的显示
+      request.setDestinationInExternalPublicDir(path,filename)--设置下载路径
+      request.setVisibleInDownloadsUi(true)--下载的文件可以被系统的Downloads应用扫描到并管理
+      request.setTitle(filename)--设置通知栏标题
+      request.setDescription("将下载到"..path)--设置通知栏消息
+      request.setShowRunningNotification(true)--设置显示下载进度提示
+      local downloadId=downloadManager.enqueue(request)
+      local downloadManager=activity.getSystemService(Context.DOWNLOAD_SERVICE)
+      local query=DownloadManager.Query()
+      local cursor=downloadManager.query(query)
+      if(not cursor.moveToFirst())then
+        cursor.close()
+        return
+      end
+      弹出消息("开始下载，请前往通知栏查看下载进度")
+    end)
+    .setNegativeButton("外部下载",function()
+      import "android.content.Intent"
+      import "android.net.Uri"
+      activity.startActivity(Intent("android.intent.action.VIEW",Uri.parse(url)))
+    end)
+    .setNeutralButton("取消",nil)
+    .show()
+  end
+})
