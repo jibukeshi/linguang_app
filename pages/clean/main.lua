05ac2ad7781c8d9d451bce1afe4634dcc1328b92
机不可失 <126877770+jibukeshi@users.noreply.github.com>
2023-05-02 08:24:58 +0800

    Add files via upload

--- pages/clean/main.lua
+++ pages/clean/main.lua
@@ -6,6 +6,7 @@ import "android.app.*"
 import "com.google.android.material.card.MaterialCardView"
 import "com.google.android.material.button.MaterialButton"
 import "androidx.recyclerview.widget.*"
+import "androidx.core.widget.NestedScrollView"
 import "java.io.File"
 import "android.text.format.Formatter"
 
@@ -14,11 +15,11 @@ Colors=UiManager.Colors
 
 
 --颜色配置
-强调色=Colors.getColorAccent()
-主色=Colors.getColorPrimary()
-主文本色=Colors.getTextColorPrimary()
-副文本色=Colors.getTextColorSecondary()
-窗体背景色=Colors.getWindowBackground()
+colorAccent=Colors.getColorAccent()--强调色
+colorPrimary=Colors.getColorPrimary()--主色
+textColorPrimary=Colors.getTextColorPrimary()--主文本色
+textColorSecondary=Colors.getTextColorSecondary()--副文本色
+windowBackground=Colors.getWindowBackground()--窗体背景色
 
 
 layout=--常规框架
@@ -40,7 +41,7 @@ layout=--常规框架
       MaterialCardView;--卡片控件
       layout_width="fill";--卡片宽度
       layout_height="wrap";--卡片高度
-      cardBackgroundColor=主色;--卡片颜色
+      cardBackgroundColor=colorPrimary;--卡片颜色
       layout_margin="10dp";--卡片边距
       cardElevation="0dp";--卡片阴影
       radius="5dp";--卡片圆角
@@ -63,7 +64,7 @@ layout=--常规框架
       textSize="15sp";--文字大小
       textColor=卡片色;--文字颜色
       id="clean";--设置控件ID
-      backgroundColor=强调色;--背景颜色
+      backgroundColor=colorAccent;--背景颜色
       gravity="center";--重力
       --左:left 右:right 中:center 顶:top 底:bottom
       layout_margin="10dp";--控件外边距
@@ -80,7 +81,7 @@ item=
   MaterialCardView;--卡片控件
   layout_width="fill";--卡片宽度
   layout_height="wrap";--卡片高度
-  cardBackgroundColor=主色;--卡片颜色
+  cardBackgroundColor=colorPrimary;--卡片颜色
   cardElevation="0dp";--卡片阴影
   radius="5dp";--卡片圆角
   id="card";--设置控件ID
@@ -90,7 +91,7 @@ item=
     layout_height="fill";--控件高度
     text="";--显示文字
     textSize="16sp";--文字大小
-    textColor=主文本色;--文字颜色
+    textColor=textColorPrimary;--文字颜色
     layout_margin="10dp";--控件外边距
     id="check";--设置控件ID
     clickable=false;
@@ -99,23 +100,18 @@ item=
 };
 
 
-adp=LuaRecyclerAdapter(activity,item).setClickViewId("card")
-recycler.setLayoutManager(LinearLayoutManager(activity))
-recycler.setAdapter(adp)
-
-ExternalFilesDir=activity.getExternalFilesDir(nil).toString()--获取外部数据目录
-ExternalCacheDir=activity.getExternalCacheDir().toString()--获取外部缓存目录
-DataDir=activity.getDataDir().toString()--获取数据目录
+externalFilesDir=activity.getExternalFilesDir(nil).toString()--获取外部数据目录
+externalCacheDir=activity.getExternalCacheDir().toString()--获取外部缓存目录
+dataDir=activity.getDataDir().toString()--获取数据目录
 
 data={
-  {title="应用崩溃记录",dir=ExternalFilesDir.."/crash"},
-  {title="SVG 图片缓存",dir=ExternalFilesDir.."/svg"},
-  {title="Glide 图片加载缓存",dir=DataDir.."/cache/image_manager_disk_cache"},
-  {title="WebView 存储数据",dir=DataDir.."/app_webview"},
-  {title="WebView 缓存信息",dir=DataDir.."/cache/WebView"},
-  {title="其他缓存",dir=ExternalCacheDir},
+  {title="应用崩溃记录",dir=externalFilesDir.."/crash",check=true},
+  {title="SVG 图片缓存",dir=externalFilesDir.."/svg",check=true},
+  {title="Glide 图片加载缓存",dir=dataDir.."/cache/image_manager_disk_cache",check=true},
+  {title="WebView 存储数据",dir=dataDir.."/app_webview",check=false},
+  {title="WebView 缓存信息",dir=dataDir.."/cache/WebView",check=false},
+  {title="其他缓存",dir=externalCacheDir,check=true},
 }
-
 
 function GetDirSize(dir)--获取目录的大小
   local size=0
@@ -150,40 +146,45 @@ function DeleteDir(dir)--删除目录
   end
 end
 
-
-function UpdateCacheList()
-  --更新RecyclerView
-  adp.clear()
-  for key,value in ipairs(data)
-    local size=Formatter.formatFileSize(activity,GetDirSize(value.dir))
-    adp.add{check=value.title.."（"..size.."）"}
-  end
-end
-UpdateCacheList()
-
-adp.onItemClick=function(adapter,itemView,view,pos)
-  local check=view.getChildAt(0)
-  check.setChecked(not check.checked)
-  local size=0
-  for i=0,adp.getItemCount()-1 do
-    local check2=recycler.getChildAt(i).getChildAt(0)
-    if(check2.checked)then
-      local current=GetDirSize(data[i+1].dir)
-      size=size+current
+function UpdateList(view,position)--更新列表
+  --获取当前项大小
+  local size=Formatter.formatFileSize(activity,GetDirSize(data[position+1].dir))
+  view.check.setText(data[position+1].title.."（"..size.."）")
+  view.check.setChecked(data[position+1].check)
+  --获取总大小
+  local total=0
+  for i=1,#data do
+    --遍历所有项目
+    if(data[i].check)then
+      --如果勾选了就加上
+      local current=GetDirSize(data[i].dir)
+      total=total+current
     end
   end
-  clean.setText("确定清理".."（"..Formatter.formatFileSize(activity,size).."）")
+  local size=Formatter.formatFileSize(activity,total)
+  clean.setText("确定清理".."（"..size.."）")
 end
+
+adp=LuaRecyclerAdapter(activity,data,item).setAdapterInterface({
+  onBindViewHolder=function(holder,position)
+    local view=holder.tag
+    UpdateList(view,position)
+    view.card.onClick=function()
+      data[position+1].check=not(data[position+1].check)
+      UpdateList(view,position)
+    end
+  end
+})
+recycler.setLayoutManager(LinearLayoutManager(activity))
+recycler.setAdapter(adp)
 
 clean.onClick=function()
-  for i=0,adp.getItemCount()-1
-    local check=recycler.getChildAt(i).getChildAt(0)
-    if(check.checked)then
+  for i=1,#data do
+    if(data[i].check)then
       --如果勾选了就清理
-      DeleteDir(data[i+1].dir)
-      check.setChecked(false)--清理完成，把勾取消
+      DeleteDir(data[i].dir)
     end
   end
-  UpdateCacheList()
-  clean.setText("确定清理（0B）")
+  adp.notifyDataSetChanged()
+  Toast.makeText(activity,"清理成功",0).show()
 end
