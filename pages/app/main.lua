29628c74bf35c2617ddfa182eab07841d306fcf6
机不可失 <126877770+jibukeshi@users.noreply.github.com>
2023-05-02 08:24:22 +0800

    Add files via upload

--- pages/app/main.lua
+++ pages/app/main.lua
@@ -20,17 +20,17 @@ Colors=UiManager.Colors
 
 
 --颜色配置
-强调色=Colors.getColorAccent()
-主色=Colors.getColorPrimary()
-主文本色=Colors.getTextColorPrimary()
-副文本色=Colors.getTextColorSecondary()
-窗体背景色=Colors.getWindowBackground()
+colorAccent=Colors.getColorAccent()--强调色
+colorPrimary=Colors.getColorPrimary()--主色
+textColorPrimary=Colors.getTextColorPrimary()--主文本色
+textColorSecondary=Colors.getTextColorSecondary()--副文本色
+windowBackground=Colors.getWindowBackground()--窗体背景色
 
 
 --获取主页面传递过来的链接
 url=...
 --服务器配置
-server="https://linguang.top/"
+server="https://cdn.bwcxlg.top/"
 
 
 -- @param title 点击的菜单标题
@@ -38,10 +38,12 @@ server="https://linguang.top/"
 function onMenuItemClick(title)
   switch title
    case "分享应用"
-    分享文本(server..url)
+    import "androidx.core.app.ShareCompat"
+    ShareCompat.IntentBuilder.from(activity).setText(server..url).setType("text/plain").startChooser()
    case "复制链接"
-    复制文本(server..url)
-    弹出消息("链接已复制到剪贴板")
+    import "android.content.Context"
+    activity.getSystemService(Context.CLIPBOARD_SERVICE).setText(server..url)
+    Toast.makeText(activity,"链接已复制到剪贴板",0).show()
   end
 end
 
@@ -81,7 +83,7 @@ layout=--常规框架
       layout_height="wrap";--控件高度
       text="获取中……";--显示文字
       textSize="16sp";--文字大小
-      textColor=主文本色;--文字颜色
+      textColor=textColorPrimary;--文字颜色
       id="message";--设置控件ID
       --singleLine=true;--设置单行输入
       --ellipsize="end";--多余文字用省略号显示
@@ -120,7 +122,7 @@ layout=--常规框架
           MaterialCardView;--卡片控件
           layout_width="100dp";--卡片宽度
           layout_height="100dp";--卡片高度
-          cardBackgroundColor=主色;--卡片颜色
+          cardBackgroundColor=colorPrimary;--卡片颜色
           layout_margin="10dp";--卡片边距
           cardElevation="0dp";--卡片阴影
           radius="5dp";--卡片圆角
@@ -162,7 +164,7 @@ layout=--常规框架
             layout_height="wrap";--控件高度
             text="";--显示文字
             textSize="22sp";--文字大小
-            textColor=主文本色;--文字颜色
+            textColor=textColorPrimary;--文字颜色
             id="name";--设置控件ID
             --singleLine=true;--设置单行输入
             --ellipsize="end";--多余文字用省略号显示
@@ -200,7 +202,7 @@ layout=--常规框架
             textSize="15sp";--文字大小
             textColor=卡片色;--文字颜色
             id="button";--设置控件ID
-            backgroundColor=强调色;--背景颜色
+            backgroundColor=colorAccent;--背景颜色
             gravity="center";--重力
             --左:left 右:right 中:center 顶:top 底:bottom
           };
@@ -210,7 +212,7 @@ layout=--常规框架
         MaterialCardView;--卡片控件
         layout_width="fill";--卡片宽度
         layout_height="wrap";--卡片高度
-        cardBackgroundColor=主色;--卡片颜色
+        cardBackgroundColor=colorPrimary;--卡片颜色
         layout_margin="10dp";--卡片边距
         cardElevation="0dp";--卡片阴影
         radius="5dp";--卡片圆角
@@ -265,7 +267,7 @@ layout=--常规框架
         MaterialCardView;--卡片控件
         layout_width="fill";--卡片宽度
         layout_height="wrap";--卡片高度
-        cardBackgroundColor=主色;--卡片颜色
+        cardBackgroundColor=colorPrimary;--卡片颜色
         layout_margin="10dp";--卡片边距
         cardElevation="0dp";--卡片阴影
         radius="5dp";--卡片圆角
@@ -288,7 +290,7 @@ layout=--常规框架
           layout_height="wrap";--控件高度
           text="";--显示文字
           textSize="16sp";--文字大小
-          textColor=主文本色;--文字颜色
+          textColor=textColorPrimary;--文字颜色
           id="slogan";--设置控件ID
           --singleLine=true;--设置单行输入
           --ellipsize="end";--多余文字用省略号显示
@@ -307,7 +309,7 @@ layout=--常规框架
         layout_height="wrap";--控件高度
         text="应用预览";--显示文字
         textSize="18sp";--文字大小
-        textColor=主文本色;--文字颜色
+        textColor=textColorPrimary;--文字颜色
         --id="Text";--设置控件ID
         --singleLine=true;--设置单行输入
         --ellipsize="end";--多余文字用省略号显示
@@ -334,7 +336,7 @@ layout=--常规框架
         layout_height="wrap";--控件高度
         text="应用介绍";--显示文字
         textSize="18sp";--文字大小
-        textColor=主文本色;--文字颜色
+        textColor=textColorPrimary;--文字颜色
         --id="Text";--设置控件ID
         --singleLine=true;--设置单行输入
         --ellipsize="end";--多余文字用省略号显示
@@ -353,7 +355,7 @@ layout=--常规框架
         layout_height="wrap";--控件高度
         text="";--显示文字
         textSize="16sp";--文字大小
-        textColor=主文本色;--文字颜色
+        textColor=textColorPrimary;--文字颜色
         id="description";--设置控件ID
         --singleLine=true;--设置单行输入
         --ellipsize="end";--多余文字用省略号显示
@@ -371,7 +373,7 @@ layout=--常规框架
         layout_height="wrap";--控件高度
         text="更新日志";--显示文字
         textSize="18sp";--文字大小
-        textColor=主文本色;--文字颜色
+        textColor=textColorPrimary;--文字颜色
         id="updateTitle";--设置控件ID
         --singleLine=true;--设置单行输入
         --ellipsize="end";--多余文字用省略号显示
@@ -390,7 +392,7 @@ layout=--常规框架
         layout_height="wrap";--控件高度
         text="";--显示文字
         textSize="16sp";--文字大小
-        textColor=主文本色;--文字颜色
+        textColor=textColorPrimary;--文字颜色
         id="update";--设置控件ID
         --singleLine=true;--设置单行输入
         --ellipsize="end";--多余文字用省略号显示
@@ -408,7 +410,7 @@ layout=--常规框架
         layout_height="wrap";--控件高度
         text="应用信息";--显示文字
         textSize="18sp";--文字大小
-        textColor=主文本色;--文字颜色
+        textColor=textColorPrimary;--文字颜色
         --id="Text";--设置控件ID
         --singleLine=true;--设置单行输入
         --ellipsize="end";--多余文字用省略号显示
@@ -427,7 +429,7 @@ layout=--常规框架
         layout_height="wrap";--控件高度
         text="";--显示文字
         textSize="16sp";--文字大小
-        textColor=主文本色;--文字颜色
+        textColor=textColorPrimary;--文字颜色
         id="information";--设置控件ID
         --singleLine=true;--设置单行输入
         --ellipsize="end";--多余文字用省略号显示
@@ -445,7 +447,7 @@ layout=--常规框架
         layout_height="wrap";--控件高度
         text="应用权限";--显示文字
         textSize="18sp";--文字大小
-        textColor=主文本色;--文字颜色
+        textColor=textColorPrimary;--文字颜色
         --id="Text";--设置控件ID
         --singleLine=true;--设置单行输入
         --ellipsize="end";--多余文字用省略号显示
@@ -464,7 +466,7 @@ layout=--常规框架
         layout_height="wrap";--控件高度
         text="";--显示文字
         textSize="16sp";--文字大小
-        textColor=主文本色;--文字颜色
+        textColor=textColorPrimary;--文字颜色
         id="permission";--设置控件ID
         --singleLine=true;--设置单行输入
         --ellipsize="end";--多余文字用省略号显示
@@ -482,7 +484,7 @@ layout=--常规框架
         layout_height="wrap";--控件高度
         text="应用评论";--显示文字
         textSize="18sp";--文字大小
-        textColor=主文本色;--文字颜色
+        textColor=textColorPrimary;--文字颜色
         --id="Text";--设置控件ID
         --singleLine=true;--设置单行输入
         --ellipsize="end";--多余文字用省略号显示
@@ -500,7 +502,7 @@ layout=--常规框架
         layout_width="fill";--浏览器宽度
         layout_height="fill";--浏览器高度
         id="comment";--控件ID
-        background=窗体背景色;--控件背景
+        background=windowBackground;--控件背景
       };
     };
   };
@@ -515,8 +517,7 @@ loading.setVisibility(View.VISIBLE)
 downloadCard.setVisibility(View.GONE)
 
 Http.get(server..url,nil,"UTF-8",nil,function(code,content,cookie,header)
-  if(code==200 and content)then
-    --获取成功
+  xpcall(function()
     normal.setVisibility(View.VISIBLE)
     loading.setVisibility(View.GONE)
 
@@ -550,7 +551,7 @@ Http.get(server..url,nil,"UTF-8",nil,function(code,content,cookie,header)
       table.insert(评分数据,k:match("height:10(.-)</p>"):match([[;">(.+)]]))
     end
 
-    设置标题(页面标题)
+    UiManager.toolbar.setTitleText(页面标题)
     --判断应用图标是不是svg
     if(应用图标:find".svg")then
       --如果是svg，判断是否有缓存
@@ -588,7 +589,7 @@ Http.get(server..url,nil,"UTF-8",nil,function(code,content,cookie,header)
         layout_width="wrap";--卡片宽度
         layout_height="wrap";--卡片高度
         layout_margin="5dp";--卡片边距
-        cardBackgroundColor=主色;--卡片颜色
+        cardBackgroundColor=colorPrimary;--卡片颜色
         cardElevation="0dp";--卡片阴影
         radius="5dp";--卡片圆角
         id="card";--设置控件ID
@@ -643,11 +644,10 @@ Http.get(server..url,nil,"UTF-8",nil,function(code,content,cookie,header)
       end
     end
 
-   else
-    --获取失败
+  end,function(e)
     progress.setVisibility(View.GONE)
     message.setText("获取失败 ("..code..")")
-  end
+  end)
 end)
 
 
@@ -701,7 +701,7 @@ comment.removeView(comment.getChildAt(0))
 comment.setWebViewClient({
   shouldOverrideUrlLoading=function(view,url)
     --Url即将跳转
-    进入子页面("web",{url})
+    activity.newActivity("web",{url})
     return true
   end,
   onPageStarted=function(view,url,favicon)
@@ -840,7 +840,7 @@ function 安装应用(path)
     activity.startActivity(intent)
    else
     --如果没有权限就申请
-    弹出消息("需要给予安装与存储读取权限")
+    Toast.makeText(activity,"需要给予安装与存储读取权限",0).show()
     import "android.content.Intent"
     import "android.provider.Settings"
     import "android.net.Uri"
